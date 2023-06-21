7e69166f5ea8f055e5d0f231598c58c50e654e66
Sandeep Sokle <sandeepsokle12@gmail.com>
2023-06-21 12:20:52 +0530

    sandeep || add socket on frontend

--- src/pages/admin/LogoutFunction.js
+++ src/pages/admin/LogoutFunction.js
@@ -1,31 +1,28 @@
-
 import axios from "axios";
-export const deleteToken = async(navigate,toast) =>{
-console.log("hii yha ye b chal rha h ");
+import { apiUrl } from "../../services/contants";
+export const deleteToken = async (navigate, toast) => {
+  const userEmail = localStorage.getItem("userEmail");
+  localStorage.removeItem("userRole");
+  localStorage.removeItem("userName");
+  localStorage.removeItem("userToken");
 
-   const userEmail =  localStorage.getItem('userEmail')
-    localStorage.removeItem('userRole');
-    localStorage.removeItem('userName');
-    localStorage.removeItem("userToken");
+  try {
+    const userData = await axios.put(`${apiUrl}/user/updatebyadmin`, {
+      _id: userEmail,
+      isAuthentify: false,
+    });
 
-    try {
-        const userData = await axios.put("http://localhost:8080/user/updatebyadmin",{
-            _id:userEmail,
-            isAuthentify:false
-        })
-
-        if(userData){
-            toast({
-                title: 'Logout SuccessFull',
-                description: "Logout",
-                status: 'success',
-                isClosable: true,
-              })
-        }
+    if (userData) {
+      toast({
+        title: "Logout SuccessFull",
+        description: "Logout",
+        status: "success",
+        isClosable: true,
+      });
     }
-     catch (error) {
-        console.log(error);
-    }
-    localStorage.removeItem('userEmail');
-    navigate("/admin/login");
-} 
+  } catch (error) {
+    console.log(error);
+  }
+  localStorage.removeItem("userEmail");
+  navigate("/admin/login");
+};
