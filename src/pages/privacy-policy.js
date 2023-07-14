ce9f1f729cff0fc04e3ef227c7e7b2bfadd73f85
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-07-14 09:56:55 +0530

    update the new page(privacy policy)

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -17,7 +17,7 @@ export default function PrivacyPolicy({ services, seotags }) {
       <NavbarHome services={services} />
       <div className="row p-0 m-0">
         <div className="col set-Back">
-          <h1 className="set-contact">Privacy Policy</h1>
+          <h1 className="set-privacy-policy">Privacy Policy</h1>
         </div>
       </div>
 
