9b317f58ef7a554200c9f597889d28e5efc83b75
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-07-28 23:28:22 +0530

    fix the logout functionality

--- src/pages/service/[slug].js
+++ src/pages/service/[slug].js
@@ -720,11 +720,6 @@ export async function getStaticProps({ params }) {
   const { data: faqschemasData } = await client.query({
     query: FAQSCHEMA,
   });
-  // console.log({
-  //   data: data.services,
-  //   serviceData: serviceData.services,
-  //   faqschemasData: faqschemasData.faqschemas,
-  // });
 
   return {
     props: {
--- src/pages/verify/[id].js
+++ src/pages/verify/[id].js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { apiUrl } from "../../services/contants";
+import { SERVICES, apiUrl } from "../../services/contants";
+import { NavbarHome } from "../../components/home_components/navbar_home";
+import { client } from "../_app";
+import Head from "next/head";
 
-function verify() {
+function verify({ services }) {
   const [isVerified, setIsverified] = useState(false);
   const [userEmail, setUserEmail] = useState("");
   const [browserId, setBrowserId] = useState("");
@@ -14,7 +17,7 @@ function verify() {
       token: navigate.query.id,
       isAuthentify: true,
     });
-    console.log({ data });
+    console.log({ data, navigate });
     if (data.data.success === true) {
       setIsverified(true);
       setUserEmail(data.data.response.email);
@@ -28,33 +31,67 @@ function verify() {
   }, [navigate]);
 
   return (
-    <div className={isVerified === true ? "verifydiv" : "verifydivred"}>
-      <div
-        className={isVerified === true ? "miniverifydiv" : "miniverifydivred"}
-      >
-        <img
-          src={
-            isVerified === true
-              ? "/assets/verified.png"
-              : "/assets/notverify.png"
-          }
-        />
+    <>
+      <Head>
+        <title>Verify</title>
+      </Head>
+      <NavbarHome services={services} />
+      <div className={isVerified === true ? "verifydiv" : "verifydivred"}>
         <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          className={isVerified === true ? "wordblue" : "wordred"}
+          className={isVerified === true ? "miniverifydiv" : "miniverifydivred"}
         >
-          <div>Email : {userEmail}</div>
-          <div>BrowserId : {browserId}</div>
-          <div>Verfied</div>
+          <img
+            src={
+              isVerified === true
+                ? "/assets/verified.png"
+                : "/assets/notverify.png"
+            }
+          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            className={isVerified === true ? "wordblue" : "wordred"}
+          >
+            <div>Email : {userEmail}</div>
+            <div>BrowserId : {browserId}</div>
+            <div>Verfied</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
 export default verify;
+
+export async function getStaticPaths() {
+  const paths = [
+    {
+      params: {
+        id: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJNYW5zaWdoYW5nYTk5N0BnbWFpbC5jb20iLCJicm93c2VyVG9rZW4iOiJBU1NJR05NRU5UXzE2OTA1NTg2MTc2MzMiLCJpYXQiOjE2OTA1NTg2MTh9.V2TOprA_ajeoA6R0iTWDtcrYlP-59Fi_vMifi4u23og",
+      },
+    },
+  ];
+  console.log({ paths });
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps() {
+  const { data: serviceData } = await client.query({
+    query: SERVICES,
+  });
+
+  return {
+    props: {
+      services: serviceData.services,
+    },
+    revalidate: 1,
+  };
+}
