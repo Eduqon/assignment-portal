eb8db055a4b9140b2ad97b8014e9d41ed40b99cf
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-07-28 15:47:43 +0530

    fix the logout functionality

--- src/pages/admin/LogoutFunction.js
+++ src/pages/admin/LogoutFunction.js
@@ -1,36 +1,6 @@
 import axios from "axios";
 import { apiUrl } from "../../services/contants";
 
-export const deleteToken = async (navigate, toast) => {
-  const userEmail = localStorage.getItem("userEmail");
-  localStorage.removeItem("userName");
-
-  try {
-    let assignmentSantaBrowserToken = localStorage.getItem(
-      "assignmentSantaBrowserToken"
-    );
-
-    const userData = await axios.put(`${apiUrl}/user/logout`, {
-      _id: userEmail,
-      browserId: assignmentSantaBrowserToken,
-      isAuthentify: false,
-    });
-
-    if (userData) {
-      toast({
-        title: "Logout SuccessFull",
-        description: "Logout",
-        status: "success",
-        isClosable: true,
-      });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-  localStorage.removeItem("userEmail");
-  navigate("/admin/login");
-};
-
 export const logoutUser = async ({ res, toast, setRefresh }) => {
   try {
     let userToken = localStorage.getItem("userToken");
