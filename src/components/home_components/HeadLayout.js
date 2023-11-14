42a76046159793188f5eeba27627e5a157e73a32
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-11-14 08:54:59 +0530

    apply console to fix the build issue

--- src/components/home_components/HeadLayout.js
+++ src/components/home_components/HeadLayout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 
 const HeadLayout = ({ slug, seotags }) => {
-  console.log({ seotags, slug });
+  // console.log({ seotags, slug });
   const homeTags =
     seotags &&
     seotags.data.length !== 0 &&
@@ -14,6 +14,7 @@ const HeadLayout = ({ slug, seotags }) => {
     homeTags.length !== 0 && homeTags[0].attributes.description;
   const keyword = homeTags.length !== 0 && homeTags[0].attributes.keyword;
   const canonicalURL = homeTags.length !== 0 && homeTags[0].attributes.cntag;
+  console.log({ title, description, keyword, canonicalURL });
   const data = {
     "@context": "https://schema.org",
     "@type": "Organization",
