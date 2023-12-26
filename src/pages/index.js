74b94ab9912d9b3c0c55cc954bc9b3d5543823e3
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-12-26 22:31:54 +0530

    performance optimization

--- src/pages/index.js
+++ src/pages/index.js
@@ -260,8 +260,7 @@ function Home({ services, seotags, reviews }) {
           <Box display={"flex"} justifyContent={"center"}>
             <Box width={["8%", "8%", "8%", "6%"]}>
               <Image
-                width={"auto"}
-                height={"auto"}
+                layout="fill"
                 src="/assets/icons/assignment-santa-lines.png"
                 alt="assignment santa line"
               />
@@ -495,8 +494,7 @@ function Home({ services, seotags, reviews }) {
               display={["none", "none", "block", "block"]}
             >
               <Image
-                width={"auto"}
-                height={"auto"}
+                layout="fill"
                 src="/assets/icons/Santa_home.png"
                 alt="Assignment Santa"
               />
@@ -550,12 +548,7 @@ function Home({ services, seotags, reviews }) {
                     gap={"1rem"}
                   >
                     <Box width={["30%", "30%", "30%", "15%"]}>
-                      <Image
-                        width={"auto"}
-                        height={"auto"}
-                        src={data.icon}
-                        alt={data.altText}
-                      />
+                      <Image layout="fill" src={data.icon} alt={data.altText} />
                     </Box>
                     <Box width={["70%", "70%", "70%", "80%"]}>
                       <h3
@@ -642,12 +635,7 @@ function Home({ services, seotags, reviews }) {
                     background={"#ffd9df"}
                     padding={"0.4rem"}
                   >
-                    <Image
-                      width={"auto"}
-                      height={"auto"}
-                      src={data.icon}
-                      alt={data.altText}
-                    />
+                    <Image layout="fill" src={data.icon} alt={data.altText} />
                   </Box>
                   <Box>
                     <Heading size="md">{data.heading}</Heading>
@@ -701,12 +689,7 @@ function Home({ services, seotags, reviews }) {
                     background={"#ffd9df"}
                     padding={"0.4rem"}
                   >
-                    <Image
-                      width={"auto"}
-                      height={"auto"}
-                      src={data.icon}
-                      alt={data.altText}
-                    />
+                    <Image layout="fill" src={data.icon} alt={data.altText} />
                   </Box>
                   <Box>
                     <Heading size="md">{data.heading}</Heading>
@@ -1445,8 +1428,7 @@ function Home({ services, seotags, reviews }) {
             display={["none", "none", "none", "block"]}
           >
             <Image
-              width={"auto"}
-              height={"auto"}
+              layout="fill"
               src="/assets/newDesigns/FooterSanta.png"
               alt="Footer Santa"
             />
@@ -1495,8 +1477,7 @@ function Home({ services, seotags, reviews }) {
             transform={["scale(1)", "scale(1.1)"]}
           >
             <Image
-              width={"auto"}
-              height={"auto"}
+              layout="fill"
               src="/assets/newDesigns/Gifts.png"
               alt="Gifts"
             />
